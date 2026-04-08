80c4fc033d347e8a79aedcafdf93ac1b6c2b63a2
LucasCoutoDuarte <lucascoutoduarte@gmail.com>
2026-04-08 09:27:15 -0300

    come

--- src/validacao/post.js
+++ src/validacao/post.js
@@ -1,5 +1,20 @@
 const Joi = require('joi');
 
+const schemaPost = Joi.object({
+    titulo: Joi.string().min(3).max(100).required().messages({
+        "string.empty": 'O título é obrigatório',
+        "string.min": 'O título deve conter pelo menos 3 caracteres',
+        "string.max": 'O título deve conter no máximo 100 caracteres',
+        "any.required": 'O título é obrigatório'
+    }),
+    conteudo: Joi.string().min(10).max(1000).required().messages({
+        "string.empty": 'O conteúdo é obrigatório',
+        "string.min": 'O conteúdo deve conter pelo menos 10 caracteres',
+        "string.max": 'O conteúdo deve conter no máximo 1000 caracteres',
+        "any.required": 'O conteúdo é obrigatório'
+    })
+});
+
 function validarPost(req, res, next) {
     const { error } = schemaPost.validate(req.body, { abortEarly: false });
     if (error) {
@@ -8,4 +23,4 @@ function validarPost(req, res, next) {
     next();
 }
 
-module.exports =  validarPost;
+module.exports = validarPost;
